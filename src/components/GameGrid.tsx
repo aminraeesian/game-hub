e05a4441f95791e71f449f445b5f4865f2867ef9
Amin <amin.raeesian@gmail.com>
2024-03-05 18:22:32 +0330

    Build platform selector

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -19,7 +19,7 @@ const GameGrid = ({ selectedGenre }: Props) => {
       <SimpleGrid
         columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
         spacing={3}
-        paddingX={5}>
+        padding={5}>
         {isloading &&
           skeletons.map((skeleton) => (
             <GameCardContainer key={skeleton}>
